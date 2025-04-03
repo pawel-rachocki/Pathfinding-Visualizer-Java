d4f4e045301464d8590fd6c5d3e78342b418404b
Pawel <pawel.logo@gmail.com>
2025-04-04 01:07:34 +0200

    connected gui class to Main

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,27 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        PathFindApp.launch(PathFindApp.class,args);
 
         //Just for testing
-        Grid grid = new Grid(20, 20);
-
-        grid.setStartNode(0, 0, true);
-        grid.setEndNode(19, 19, true);
-
-        grid.generateRandomObstacles(0.3);
-
-        AStarPathfinder pathfinder = new AStarPathfinder(grid,HeuristicType.MANHATTAN);
-        List<Node> path = pathfinder.findPath();
-        if (!path.isEmpty()) {
-            System.out.println("Znaleziono ścieżkę:");
-            for (Node node : path) {
-                System.out.println(" -> (" + node.getX() + ", " + node.getY() + ")");
-            }
-        } else {
-            System.out.println("Nie znaleziono ścieżki.");
-        }
-        System.out.println(grid);
+//        Grid grid = new Grid(20, 20);
+//
+//        grid.setStartNode(0, 0, true);
+//        grid.setEndNode(19, 19, true);
+//
+//        grid.generateRandomObstacles(0.3);
+//
+//        AStarPathfinder pathfinder = new AStarPathfinder(grid,HeuristicType.MANHATTAN);
+//        List<Node> path = pathfinder.findPath();
+//        if (!path.isEmpty()) {
+//            System.out.println("Znaleziono ścieżkę:");
+//            for (Node node : path) {
+//                System.out.println(" -> (" + node.getX() + ", " + node.getY() + ")");
+//            }
+//        } else {
+//            System.out.println("Nie znaleziono ścieżki.");
+//        }
+//        System.out.println(grid);
 
     }
 }
